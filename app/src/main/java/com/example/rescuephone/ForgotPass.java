1eb3314405a6821db93f443b8d18e19fccf92efa
Minerva Araceli Rivera Arias <mine.rivera.arias@gmail.com>
2023-07-25 19:28:19 -0600

    Registro completo

--- app/src/main/java/com/example/rescuephone/ForgotPass.java
+++ app/src/main/java/com/example/rescuephone/ForgotPass.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Perfil_usu extends AppCompatActivity {
+public class ForgotPass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil_usu);
+        setContentView(R.layout.activity_forgot_pass);
     }
 }
